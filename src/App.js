f07eb8fdef472cfaeafbf298840c567c03351e43
uaysev <utkua557@gmail.com>
2024-07-31 19:29:10 -0400

    initial commit for react quiz-frontend project.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+
+function App() {
+    return (
+        <div>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/services/authService.js
@@ -0,0 +1,8 @@
+// Placeholder for authentication service
+export const login = (credentials) => {
+    // API call for login
+};
+
+export const register = (userData) => {
+    // API call for registration
+};
